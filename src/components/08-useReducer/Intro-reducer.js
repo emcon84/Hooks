eaf359ce6f4a674f76b34d6d3e03764cdf4a2245
Emiliano Conti <emcon84@gmail.com>
2021-10-27 22:20:49 -0300

    updates

---
+++ src/components/08-useReducer/Intro-reducer.js
@@ -0,0 +1,13 @@
+const initialState = [{
+    id: 1,
+    todo: 'Comprar Pan',
+    done: false
+}];
+
+const todoReducer = (state = initialState, action) => {
+    return state;
+}
+
+let todos = todoReducer();
+
+console.log(todos);
