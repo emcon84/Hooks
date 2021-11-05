13c20b04045eea6f020aa28601f6b9efc75a28cd
emcon84 <emcon84@gmail.com>
2021-11-05 16:34:39 -0300

    updates

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,9 +1,9 @@
-import React, { useReducer,useEffect } from 'react'
+import React, { useReducer, useEffect } from 'react'
 import { todoReducer } from './todoReducer'
-import { useForm } from '../../hooks/useForm';
+import { TodoList } from './TodoList';
 
 import './styles.css'
-
+import { TodoAdd } from './TodoAdd';
 
 const init = () => {
     return JSON.parse(localStorage.getItem('todos')) || [];
@@ -11,15 +11,10 @@ const init = () => {
 
 export const TodoApp = () => {
 
-
-    const [ todos, dispatch ] = useReducer( todoReducer, [], init );
-
-    const [ { description }, handleInputChange, reset ] = useForm({
-        description: ''
-    });
+    const [todos, dispatch] = useReducer(todoReducer, [], init);    
 
     useEffect(() => {
-       localStorage.setItem('todos', JSON.stringify(todos));
+        localStorage.setItem('todos', JSON.stringify(todos));
     }, [todos]);
 
     const handleDelete = (todoId) => {
@@ -35,98 +30,43 @@ export const TodoApp = () => {
 
     const handleToggle = (todoId) => {
         
-
-        dispatch ({  
+        dispatch({
             type: 'toggle',
             payload: todoId
-          })
+        })
     }
 
-    console.log(description);
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        const newTodo = {
-            id: new Date().getTime(),
-            desc: description,
-            done: false
-        };
-
-        const action = {
+    const handleAddTodo = (newTodo) => {
+        dispatch({
             type: 'add',
             payload: newTodo
-        };
-
-        dispatch(action);
-        reset();
+        })
     }
 
+      
 
     return (
         <div>
-            
-            <h1>Todo App ({ todos.length })</h1>
+
+            <h1>Todo App ({todos.length})</h1>
             <hr />
-
             <div className="row">
-
                 <div className="col-md-7">
-
-                    <ul className="list-group list-group-flush">
-                        {
-                            todos.map( (todo, i) => (
-                                <li 
-                                    key={ todo.id }
-                                    className="list-group-item"
-                                >                            
-                                    <p
-                                        className={ `${ todo.done &&'complete' } ` }
-                                        onClick={ () => handleToggle(todo.id) }
-                                    >
-                                        { i+1 }. { todo.desc }
-                                    </p> 
-
-                                    <button 
-                                        className="btn btn-danger"
-                                        onClick={ () => handleDelete(todo.id) }
-                                    >
-                                        Borrar
-                                    </button>
-                                </li>
-                            ))
-                        }
-                    </ul>
-
+                    <TodoList
+                        todos={todos}
+                        handleDelete={handleDelete}
+                        handleToggle={handleToggle}
+                    />
                 </div>
 
                 <div className="col-md-5">
-                    <h4>Agregar Tarea</h4>
-                    <hr />
-                    <form onSubmit={ handleSubmit }>
-                        <input 
-                            type="text"
-                            name="description"
-                            className="form-control"
-                            placeholder="Agregar una tarea"
-                            autoComplete="off"
-                            value={ description }
-                            onChange={ handleInputChange }                            
-                        />
-                        <button
-                            type="submit"
-                            className="btn btn-outline-primary mt-3 btn-block"
-                        >
-                            Agrear Tarea
-                        </button>
-                    </form>
-
+                    <TodoAdd handleAddTodo={ handleAddTodo }/>
                 </div>
 
             </div>
 
-            
-                       
+
+
         </div>
     )
 }
